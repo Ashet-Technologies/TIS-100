b5ef470ebdebef0052a33feef1fffa5c62e3f69b
Felix Queißner <git@mq32.de>
2016-09-18 15:53:56 +0200

    Improves VHDL implementation: Now supports first three instructions: NOP, SWP, SAV.

--- impl/testbench.vhdl
+++ impl/testbench.vhdl
@@ -17,9 +17,23 @@ architecture default of testbench is
 			dataout : out std_logic_vector
 		);
 	end component;
-
-	procedure tick (signal clk : out std_logic) is
 	
+	component tis50 is
+		port(
+			clk        : in  std_logic;
+			rst        : in  std_logic;
+			io_in      : in  std_logic_vector(7 downto 0);
+			io_read    : in  std_logic;
+			io_out     : in  std_logic_vector(7 downto 0);
+			io_write   : in  std_logic;
+			mem_addr   : out std_logic_vector(7 downto 0);
+			mem_val    : in  std_logic_vector(7 downto 0);
+			mem_enable : out std_logic;
+			mem_ready  : in  std_logic
+			);
+	end component;
+
+	procedure tick (signal clk : out std_logic) is	
 	begin
 		clk <= '1';
 		wait for 1 ns;
@@ -27,47 +41,90 @@ architecture default of testbench is
 		wait for 1 ns;
 	end procedure;
 
-	signal clk : std_logic := '0';
-	signal we : std_logic;
+	signal clk         : std_logic := '0';
 
+	signal ram_we      : std_logic := '0';
 	signal ram_address : std_logic_vector(7 downto 0);
-	signal ram_input : std_logic_vector(7 downto 0);
-	signal ram_output : std_logic_vector(7 downto 0);
+	signal ram_input   : std_logic_vector(7 downto 0);
+	signal ram_output  : std_logic_vector(7 downto 0);
+	signal ram_enable  : std_logic := '0';
+	signal ram_ready   : std_logic := '0';
 	
+	signal mem_address : std_logic_vector(7 downto 0);
+	
+	signal port_reg    : std_logic_vector(7 downto 0);	
+	signal port_read   : std_logic := '0';
+	signal port_write  : std_logic := '0';
+
 begin
 
 	ram0: ram port map (
 		clock   => clk, 
-		we      => we,
+		we      => ram_we,
 		address => ram_address,
 		datain  => ram_input,
 		dataout => ram_output
 	);
+	
+	tis0 : tis50 port map (
+		clk        => clk,
+		rst        => '0',
+		io_in      => port_reg,
+		io_out     => port_reg,
+		io_read    => port_read,
+		io_write   => port_write,
+		mem_addr   => mem_address,
+		mem_val    => ram_output,
+		mem_enable => ram_enable,
+		mem_ready  => ram_ready
+	);
 
 	process
+		procedure write(
+			signal clk : out std_logic;
+			address : in integer;
+			value : in integer) is
+		begin
+			ram_input   <= std_logic_vector(to_unsigned(value, ram_input'length));
+			ram_address <= std_logic_vector(to_unsigned(address, ram_address'length));
+			ram_we <= '1';
+			tick(clk => clk);
+			ram_we <= '0';
+		end procedure;
 	begin
 		wait for 1 ns;
 		
-		ram_input <= "00000010";
-		ram_address <= "00000000";
-		we <= '1';
+		write(
+			clk => clk, 
+			address => 0, 
+			value => 1); -- SWP
+		write(
+			clk => clk, 
+			address => 1, 
+			value => 2); -- SAV
+		write(
+			clk => clk, 
+			address => 2, 
+			value => 0); 
+		write(
+			clk => clk, 
+			address => 3, 
+			value => 0);
 		
-		tick(clk => clk);
-		
-		ram_input <= "00000100";
-		ram_address <= "00000001";
-		we <= '1';
-		
-		tick(clk => clk);
-		
-		we <= '0';
-		ram_address <= "00000000";
-		
-		tick(clk => clk);
-		we <= '0';
-		ram_address <= "00000001";
-		
-		tick(clk => clk);
+		for i in 0 to 13 loop
+			if ram_enable = '1' then
+				ram_address <= mem_address;
+				ram_ready <= '1';
+			else
+				ram_ready <= '0';
+			end if;
+			
+			if port_write = '1' then
+				report "Writing " & integer'image(to_integer(signed(port_reg))) & " to the port";
+			end if;
+
+			tick(clk => clk);
+		end loop;
 		
 		-- assert false report "end of test" severity note;
 		wait;
--- impl/tis50.vhdl
+++ impl/tis50.vhdl
@@ -5,14 +5,16 @@ use ieee.numeric_std.all;
 entity tis50 is
 
 	port(
-		clk        : in    std_logic;
-		rst        : in    std_logic;
-		io_in      : in    std_logic_vector(7 downto 0);
-		io_out     : in    std_logic_vector(7 downto 0);
-		mem_addr   : out   std_logic_vector(7 downto 0);
-		mem_val    : in    std_logic_vector(7 downto 0);
-		mem_enable : out   std_logic;
-		mem_ready  : in    std_logic
+		clk        : in  std_logic;
+		rst        : in  std_logic;
+		io_in      : in  std_logic_vector(7 downto 0);
+		io_read    : in  std_logic;
+		io_out     : in  std_logic_vector(7 downto 0);
+		io_write   : in  std_logic;
+		mem_addr   : out std_logic_vector(7 downto 0);
+		mem_val    : in  std_logic_vector(7 downto 0);
+		mem_enable : out std_logic;
+		mem_ready  : in  std_logic
 		);
 
 end tis50;
@@ -22,8 +24,19 @@ architecture standard of tis50 is
 	type fsm_state_type is (
 		Init,
 		Fetch,
+		Fetch_Ready,
 		FetchMore,
-		Decode
+		FetchMore_Ready,
+		Decode,
+		ADD_SRC,
+		ADD_IMM,
+		SUB_SRC,
+		SUB_IMM,
+		MOV_SRC,
+		MOV_IMM,
+		JRO_SRC,
+		JRO_IMM,
+		JMP
 	);
 	
 	signal IP          : std_logic_vector(7 downto 0);
@@ -34,41 +47,126 @@ architecture standard of tis50 is
 	signal instruction : std_logic_vector(3 downto 0);
 	signal info1       : std_logic_vector(3 downto 0);
 	signal info2       : std_logic_vector(7 downto 0);
+	
 	signal fsm_state   : fsm_state_type := Init;
+
+	signal current_state : integer;
 	
 begin
 
 	instruction <= instr_coded(3 downto 0);
 	info1       <= instr_coded(7 downto 4);
 
-	process (clk)
+	process (clk, rst)
 	begin
+		if rst = '1' then
+			fsm_state <= Init;
+		end if;
 		if rising_edge(clk) then
 			case fsm_state is
 				when Init =>
-					IP        <= std_logic_vector(to_unsigned(0, IP'Length));
-					ACC       <= std_logic_vector(to_unsigned(0, ACC'Length));
-					BAK       <= std_logic_vector(to_unsigned(0, BAK'Length));
-					fsm_state <= Fetch;
+					current_state <= 0;
+					IP         <= std_logic_vector(to_unsigned(0, IP'Length));
+					ACC        <= std_logic_vector(to_unsigned(204, ACC'Length));
+					BAK        <= std_logic_vector(to_unsigned(51, BAK'Length));
+					mem_enable <= '0';
+					fsm_state  <= Fetch;
 				when Fetch =>
+					current_state <= 1;
 					mem_addr    <= IP;
+					mem_enable  <= '1';
+					if mem_ready = '1' then
+						fsm_state <= Fetch_Ready;
+					end if;
+				when Fetch_Ready =>
+					current_state <= 2;
 					instr_coded <= mem_val;
 					IP          <= std_logic_vector(unsigned(IP) + to_unsigned(1, IP'Length));
-					if instruction(3) = '1' then
+					if mem_val(3) = '1' then
 						fsm_state <= FetchMore;
 					else
 						fsm_state <= Decode;
 					end if;
 				when FetchMore =>
-					mem_addr <= IP;
-					info2 <= mem_val;
-					IP <= std_logic_vector(unsigned(IP) + to_unsigned(1, IP'Length));
+					current_state <= 3;
+					mem_addr   <= IP;
+					mem_enable <= '1';
+					if mem_ready = '1' then
+						fsm_state <= FetchMore_Ready;
+					end if;
+				when FetchMore_Ready =>
+					current_state <= 4;
+					info2     <= mem_val;
+					IP        <= std_logic_vector(unsigned(IP) + to_unsigned(1, IP'Length));
 					fsm_state <= Decode;
 				when Decode =>
-					
-				
+					current_state <= 5;
+					case to_integer(unsigned(instruction)) is
+						when 0 => -- NOP
+							fsm_state <= Fetch;
+						when 1 => -- SWP
+							ACC <= BAK;
+							BAK <= ACC;
+							fsm_state <= Fetch;
+						when 2 => -- SAV
+							BAK <= ACC;
+							fsm_state <= Fetch;
+						when 3 => -- ADD <SRC>
+							fsm_state <= ADD_SRC;
+						when 4 => -- SUB <SRC>
+							fsm_state <= SUB_SRC;
+						when 5 => -- NEG
+							ACC <= std_logic_vector(-signed(ACC));
+							fsm_state <= Fetch;
+						when 6 => -- JRO <SRC>
+							fsm_state <= JRO_SRC;
+						when 8 => -- MOV <SRC>, <DST>
+							fsm_state <= MOV_SRC;
+						when 9 => -- MOV <IMM>, <DST>
+							fsm_state <= MOV_IMM;
+						when 10 => -- ADD <IMM>
+							ACC <= std_logic_vector(signed(ACC) + signed(info2));
+							fsm_state <= Fetch;
+						when 11 => -- SUB <IMM>
+							ACC <= std_logic_vector(signed(ACC) - signed(info2));
+							fsm_state <= Fetch;
+						when 12 => -- JMP, JEZ, JNZ, JGZ, JLZ
+							case info1 is
+								when "0000" => -- JMP
+									IP <= info2;
+									fsm_state <= Fetch;
+								when "0001" => -- JEZ
+									if signed(ACC) = 0 then
+										IP <= info2;
+									end if;
+									fsm_state <= Fetch;
+								when "0010" => -- JNZ
+									if signed(ACC) /= 0 then
+										IP <= info2;
+									end if;
+									fsm_state <= Fetch;
+								when "0011" => -- JGZ
+									if signed(ACC) > 0 then
+										IP <= info2;
+									end if;
+									fsm_state <= Fetch;
+								when "0100" => -- JLZ
+									if signed(ACC) < 0 then
+										IP <= info2;
+									end if;
+									fsm_state <= Fetch;
+								when others =>
+									fsm_state <= Init;
+							end case;
+						when 13 => -- JRO <IMM>
+							IP <= std_logic_vector(unsigned(IP) + unsigned(info2));
+							fsm_state <= Fetch;
+						when others =>
+							fsm_state <= Init;
+					end case;
 				when others =>
-					fsm_state <= Fetch;
+					current_state <= 99;
+					fsm_state <= Init;
 			end case;
 			
 		end if;
